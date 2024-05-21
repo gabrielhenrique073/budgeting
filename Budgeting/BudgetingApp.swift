0fdf283b8f5245e2fc8242468798f8eb8154589d
Gabriel <gabrielhenriquerabelolopes@gmail.com>
2024-05-21 15:45:47 -0400

    Initial Commit

---
+++ Budgeting/BudgetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetingApp.swift
+//  Budgeting
+//
+//  Created by Gabriel on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgeting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budgeting
+//
+//  Created by Gabriel on 5/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
